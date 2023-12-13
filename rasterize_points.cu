8a4c03b7c5f2a98fe78908d6b49bf10b80f77cdb
Sven <583225+skluge@users.noreply.github.com>
2023-12-13 23:06:20 +0100

    changed to NUM_CHANNEL directive

--- rasterize_points.cu
+++ rasterize_points.cu
@@ -66,7 +66,7 @@ RasterizeGaussiansCUDA(
   auto int_opts = means3D.options().dtype(torch::kInt32);
   auto float_opts = means3D.options().dtype(torch::kFloat32);
 
-  torch::Tensor out_color = torch::full({6, H, W}, 0.0, float_opts);
+  torch::Tensor out_color = torch::full({NUM_CHANNELS, H, W}, 0.0, float_opts);
   torch::Tensor out_depths = torch::full({1, H, W}, 0.0, float_opts);
   torch::Tensor out_amax_depth = torch::full({1, H, W}, 0.0, float_opts);
   torch::Tensor out_normals = torch::full({3, H, W}, 0.0, float_opts);
